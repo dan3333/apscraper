89051ed4ba4d087a8f6fd5c70dccb3594fcaa612
dan <danb33@gmail.com>
2019-07-15 17:21:41 -0400

    Adding the mongodb functioncality

--- test.py
+++ test.py
@@ -2,6 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.firefox.webdriver import FirefoxProfile
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
+import pymongo
+
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/", username="dan", password="")
+mydb = myclient["testdb"]
+mycol = mydb["apartments"]
+
+#mydict = { "name": "John", "address": "Highway 37" }
+
+#x = mycol.insert_one(mydict)
 
 
 profile = FirefoxProfile("/root/.mozilla/firefox/5l4f32wz.default")
@@ -24,8 +34,12 @@ for el in elements:
     print ("\nApartment:\n---------")
     for text in el.find_all("h5"):
         print ("Name: "+text.get_text())
+        name = text.get_text()
     for text in el.find_all("p"):
         print (text.get_text())
+        desc = text.get_text()
+    mydict = {"name": name, "description": desc}
+    x = mycol.insert_one(mydict)
 
 
 
